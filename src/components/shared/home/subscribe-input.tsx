ce31f5a50bf1229ba498ad2892ca0986e1c7b319
Jony Bhattacharjee <jonybhattacharjee886@gmail.com>
2023-11-28 19:56:50 +0600

    docs: :white_check_mark: Subscribe Ui

--- src/components/shared/home/subscribe-input.tsx
+++ src/components/shared/home/subscribe-input.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 const SubsCribeInput = () => {
-    return (<div>
-        Subscribe
-    </div> );
-}
- 
+  return (
+    <div className="mt-6 w-full flex  gap-4 items-center lg:mt-0">
+      <Input placeholder="your email..." className="py-8" />
+      <Button size={"lg"}>Subscribe</Button>
+    </div>
+  );
+};
+
 export default SubsCribeInput;
--- src/components/shared/home/subscribe.tsx
+++ src/components/shared/home/subscribe.tsx
@@ -5,9 +5,16 @@ import SubsCribeInput from "./subscribe-input";
 
 const Subscribe = () => {
   return (
-    <div className="container mt-20 flex flex-col md:flex-row items-center justify-center md:justify-between gap-4">
-          <SubsCribe />
-          <SubsCribeInput />
+    <div className="container mt-20 flex flex-col  items-center justify-center gap-4">
+      <h2 className="text-5xl font-bold font-Inter text-center mb-8">Get the menu of your favorite restaurants every day</h2>
+      <Image
+        src="/svg/subscribe.svg"
+        alt="subscribe"
+        width={800}
+              height={300}
+              className="object-cover object-center"
+      />
+      <SubsCribeInput />
     </div>
   );
 };
