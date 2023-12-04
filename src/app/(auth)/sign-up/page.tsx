55bd64fd8a511e6864464d068b6067c0186c26c2
Jony Bhattacharjee <jonybhattacharjee886@gmail.com>
2023-12-04 19:20:10 +0600

    refactor: Sign In and Sign Up Ui

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import SignUpForm from "./sign-up-form";
 const SignUp = () => {
   return (
-    <section className="flex h-full pt-11 justify-center items-center">
+    <section className="flex h-full px-10 md:px-0 pt-11 justify-center items-center">
       <div className="flex flex-col gap-8">
         <div className="relative w-16 h-8">
           <Image src="/svg/logo.svg" alt="logo" fill className="" />
