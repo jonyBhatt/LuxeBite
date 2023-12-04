078148727ff2c6c34ceca4f6e45f1be52d9277df
Jony Bhattacharjee <jonybhattacharjee886@gmail.com>
2023-12-04 22:00:20 +0600

    Initial next-auth

---
+++ src/utils/auth.ts
@@ -0,0 +1,18 @@
+import { AuthOptions } from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+
+export const authOptions = {
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+} satisfies AuthOptions;
